991c610f28fda97a28934e0f2247cdb93cf54eb1
prashantkawa <prashant.kawatkar@excellarate.com>
2023-05-23 15:55:07 +0530

    Saving code changes for PR comments

--- Prashant Kawatkar/Assignments/cypress/integration/assignments/GreenCart.js
+++ Prashant Kawatkar/Assignments/cypress/integration/assignments/GreenCart.js
@@ -3,10 +3,8 @@ describe('Add products to cart', function()
  
 it('Test Add to cart',function() {
 
-//cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
 cy.visit(Cypress.env('url')+'/seleniumPractise/#/')
 cy.get('.search-keyword').type('ca')
-cy.wait(2000)
 
 cy.get('.product').should('have.length',5)
 cy.get('.product:visible').should('have.length',4)
--- Prashant Kawatkar/Assignments/cypress/integration/assignments/ProceedToCheckout.js
+++ Prashant Kawatkar/Assignments/cypress/integration/assignments/ProceedToCheckout.js
@@ -1,14 +1,14 @@
 describe('Green cart proceed to checkout', function() 
 {
  
-it('Test proceed to checkout flow',function() {
+it('Test proceed to checkout',function() {
  
 cy.visit(Cypress.env('url')+'/seleniumPractise/#/')
 cy.get('.search-keyword').type('ca')
 cy.wait(2000)
 
 cy.get('.products').as('productLocator')
-cy.get('@productLocator').find('.product').each(($productElement, index, $list) => {
+cy.get('@productLocator').find('.product').each(($productElement, index, $list) => {    
  
 const textVeg=$productElement.find('h4.product-name').text()
 if(textVeg.includes('Cashews'))
@@ -20,5 +20,5 @@ cy.get('.cart-icon > img').click()
 cy.contains('PROCEED TO CHECKOUT').click()
 cy.contains('Place Order').click()
 
-}  )
-}  )
+} )
+} )
