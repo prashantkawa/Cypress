79261c8e3d1a2f5eb6733f50072f391a57c65b4e
prashantkawa <prashant.kawatkar@excellarate.com>
2023-06-26 13:17:18 +0530

    Saving code changes for BDD project

--- Prashant Kawatkar/Assignments/cypress/integration/assignments/sixthProg.js
+++
@@ -1,9 +0,0 @@
-describe('My Second Test Suite', function () {
-  it('My FirstTest case', function () {
-    //Check boxes
-    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
-
-    cy.contains('Top').click({ force: true });
-    cy.url().should('include', 'top');
-  });
-});
